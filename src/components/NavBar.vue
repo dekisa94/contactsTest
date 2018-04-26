2dd0cd4593afb48fb312160f20267d94c0c82511
dekisa94 <dsimic94@gmail.com>
2018-04-26 21:45:57 +0200

    link implementation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
-      <a class="navbar-brand" href="#">My contacts</a>
+      <router-link class="navbar-brand" to="contacts">My contacts</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">Contacts <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="#">Add contact</a>
+          <router-link class="nav-item nav-link active" to="contacts">Contacts</router-link>
+          <router-link class="nav-item nav-link" to="add-contact">Add Contact</router-link>
         </div>
       </div>
     </nav>
---
+++ src/pages/AddContact.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        AddContacts
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
