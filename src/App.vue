4e1966405a40e77493a163145f228f094810b3b0
dekisa94 <dsimic94@gmail.com>
2018-04-26 21:21:17 +0200

    router install

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <NavBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', redirect: '/contacts'},
+  {path: '/contacts', component: Contacts},
+  {path: '/add-contact', component : AddContact}
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
