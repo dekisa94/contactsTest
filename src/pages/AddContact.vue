e942c54e631364202e41a5158f7212210359508c
dekisa94 <dsimic94@gmail.com>
2018-05-14 21:59:26 +0200

    validateManual

--- src/pages/AddContact.vue
+++ src/pages/AddContact.vue
@@ -1,30 +1,49 @@
 <template>
     <div class="container">
-        <form @submit.prevent="submit">
+         <form v-validate="validationRules"> 
             <h2>{{ this.$route.params.id ? 'EDIT CONTACT' :  'ADD CONTACT'}}</h2>
             <div class="form-group">
                 <label for="name">First name</label>
                 <input 
                     v-focus
+                    v-validate
                     v-model="newContact.first_name" 
                     type="text" 
                     id="name" 
                     name="first_name" 
                     class="form-control" 
-                    required
+                    
                 />
             </div>
             <div class="form-group">
                 <label for="lastname">Last name</label>
-                <input v-model="newContact.last_name" type="text" id="lastname" name="last_name" class="form-control" required/>
+                <input 
+                    v-model="newContact.last_name" 
+                    type="text" 
+                    id="lastname" 
+                    name="last_name" 
+                    class="form-control" 
+                />
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input v-model="newContact.email" type="email" id="email" name="email" class="form-control" required/>
+                <input 
+                    v-model="newContact.email" 
+                    type="text" 
+                    id="email" 
+                    name="email" 
+                    class="form-control" 
+                />
             </div>
             <div class="form-group">
                 <label for="number">Number</label>
-                <input v-model="newContact.number" type="text" id="number" name="number" class="form-control" required/>
+                <input 
+                    v-model="newContact.number" 
+                    type="text" 
+                    id="number" 
+                    name="number" 
+                    class="form-control" 
+                />
             </div>
             <input type="submit" class="btn btn-primary" />
         </form>
@@ -36,7 +55,8 @@ import {contactService} from '../service/ContactService'
 export default {
   data(){
       return{
-          newContact: {first_name: '', last_name: '', email: '', number: ''}
+          newContact: {first_name: '', last_name: '', email: '', number: ''},
+          validationRules: {email: ['required', 'email']}
       }
   },
   methods:{
--- src/plugins/MyDirectives.js
+++ src/plugins/MyDirectives.js
@@ -5,6 +5,48 @@ const MyDirectives = {
               element.focus()
             }
           })
+
+        const RULES= {
+            REQUIRED: 'required',
+            NUMBER: 'number',
+            EMAIL: 'email'
+        }
+        const MESSAGES_CLASNAME = 'validator-messages'
+        const removeMessageErrorElement = (element) => {
+        let oldMessageElement = element.querySelector(`#${MESSAGES_CLASNAME}`)
+
+        if(oldMessageElement){
+            oldMessageElement.remove()
+        }
+    }
+
+        Vue.directive('validate', {
+            inserted: function(element, binding){
+
+                let validationRules = binding.value
+                
+                element.addEventListener('submit', (event) => {
+                    event.preventDefault()
+                    Object.keys(validationRules).forEach(key => {
+                        let input = element.querySelector(`#${key}`)
+
+                        if(!input){
+                            throw new Error(`Element for validation rule ${key} not found.`)
+                        }
+                        if(validationRules[key].indexOf(RULES.REQUIRED) > -1 && !input.value.length){
+                            let messageElement = document.createElement('div')
+                            messageElement.id= MESSAGES_CLASNAME
+                            removeMessageErrorElement(element)
+                            messageElement.innerHTML = `${key} is required`
+                            element.appendChild(messageElement)
+                        }else{
+                            removeMessageErrorElement(element)
+                        }
+                    });
+                    
+                })
+            }
+        }) 
     }
 }
 
